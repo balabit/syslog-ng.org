ab028146caf5f3c2bb7f6b8452ab90a59cbd6622
Gergely Nagy <algernon@balabit.hu>
2014-02-12 12:09:09 +0100

    js: Fix the scrollbar appearing problem
    Don't try to click on the navbar-toggle, it doesn't do the right thing.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- src/resources/public/js/syslog-ng.org.js
+++ src/resources/public/js/syslog-ng.org.js
@@ -1,7 +1,9 @@
+function scrollTo (link) {
+    var href = link.attr("href");
+    $("html,body").animate({scrollTop: $(href).offset().top - 100}, 500);
+}
+
 $(".nav-link").click(function(e) {
     e.preventDefault();
-    var link = $(this);
-    var href = link.attr("href");
-    $("html,body").animate({scrollTop: $(href).offset().top - 80}, 500);
-    link.closest(".navbar").find(".navbar-toggle:not(.collapsed)").click();
+    scrollTo($(this));
 });
