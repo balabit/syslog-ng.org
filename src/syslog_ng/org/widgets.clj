f01faaf7cbad80c8fbcdcf32250d43df5f926f19
Gergely Nagy <algernon@madhouse-project.org>
2014-02-18 08:39:50 +0100

    widgets.clj: Automatically cycle the alternate container
    Instead of hard-coding which part of the page has an alternate-container
    wrapper, cycle through all blocks of the page, alternating between no
    container and alternate-container.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/syslog_ng/org/widgets.clj
+++ src/syslog_ng/org/widgets.clj
@@ -110,51 +110,50 @@
   [:div.container-alternate body])
 
 (defn- page:features []
-  (widget:container-alternate
-   (widget:container
-    "features" "Features"
+  (widget:container
+   "features" "Features"
 
-    [:div.row.features
-     [:p.col-md-4.col-sm-6
-      [:strong "RFC3164 or RFC5424?"] [:br]
-      "Whether you want to work with legacy BSD syslog (RFC3164) or
-       the enhanced RFC5424 protocol, syslog-ng has you covered. Its
-       flexible parser can process pretty much any variant of
-       these protocols that you find in the wild."]
+   [:div.row.features
+    [:p.col-md-4.col-sm-6
+     [:strong "RFC3164 or RFC5424?"] [:br]
+     (str "Whether you want to work with legacy BSD syslog (RFC3164)
+       or the enhanced RFC5424 protocol, syslog-ng has you covered.
+       Its flexible parser can process pretty much any variant of
+       these protocols that you find in the wild.")]
 
-     [:p.col-md-4.col-sm-6
-      [:strong "You have unstructured data?"] [:br]
-      "You have data in an unstructured format? That's not a problem:
-       syslog-ng comes with a set of built-in parsers, which
-       you can combine to build very complex things."]
+    [:p.col-md-4.col-sm-6
+     [:strong "You have unstructured data?"] [:br]
+     (str "You have data in an unstructured format? That's not a
+       problem: syslog-ng comes with a set of built-in parsers, which
+       you can combine to build very complex things.")]
 
-     [:p.col-md-4.col-sm-6
-      [:strong "Are your logs all over the place?"] [:br]
-      "Even if the incoming events are all over the place, with
+    [:p.col-md-4.col-sm-6
+     [:strong "Are your logs all over the place?"] [:br]
+     (str "Even if the incoming events are all over the place, with
        syslog-ng's you can correlate events together, and transform
-       them into a much more useful
-       structure."]]
-    [:div.row.features
-     [:p.col-md-4.col-sm-6
-      [:strong "Databases, you say?"] [:br]
-      "If you need to store your log messages in a database, you don't
-       need to look any further! We have SQL (MySQL, PostgreSQL, even
-       Oracle!), MongoDB. We also support inserting messages into
-       Redis, if that's what you are after."]
+       them into a much more useful structure.")]]
 
-     [:p.col-md-4.col-sm-6
-      [:strong "Message queues?"] [:br]
-      "No problem! We support the Advanced Message Queuing
+   [:div.row.features
+    [:p.col-md-4.col-sm-6
+     [:strong "Databases, you say?"] [:br]
+     (str "If you need to store your log messages in a database, you
+       don't need to look any further! We have SQL (MySQL, PostgreSQL,
+       even Oracle!), MongoDB. We also support inserting messages into
+       Redis, if that's what you are after.")]
+
+    [:p.col-md-4.col-sm-6
+     [:strong "Message queues?"] [:br]
+     (str "No problem! We support the Advanced Message Queuing
        Protocol (AMQP) and the Simple Text Oriented Messaging
-       Protocol (STOMP) too, with more in the pipeline."]
+       Protocol (STOMP) too, with more in the pipeline.")]
 
-     [:p.col-md-4.col-sm-6
-      [:strong "You need something special?"] [:br]
-      "Even if you need something unique, there's a good chance that
-       syslog-ng, the swiss army knife (or Babel fish) of logging
+    [:p.col-md-4.col-sm-6
+     [:strong "You need something special?"] [:br]
+     (str "Even if you need something unique, there's a good chance
+       that syslog-ng, the swiss army knife (or Babel fish) of logging
        already has the tools to support you. But even if not,
        contributing is easy! With responsive users and developers all
-       around the globe."]])))
+       around the globe.")]]))
 
 (defn- page:news []
   (widget:container
@@ -184,9 +183,11 @@
    [:body
     (page:navbar)
     (page:jumbotron)
-    (page:highlights)
-    (page:features)
-    (page:news)
+
+    (map (fn [f body]
+           (f body))
+         (cycle [identity widget:container-alternate])
+         [(page:highlights) (page:features) (page:news)])
     (page:footer)
 
     (include-js "http://code.jquery.com/jquery-1.10.1.min.js")
