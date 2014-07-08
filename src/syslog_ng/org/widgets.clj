6c3fc88c1e05bd2510f7a04cdd1da3a3b6ce4c33
Gergely Nagy <algernon@madhouse-project.org>
2014-07-08 16:44:16 +0200

    Split analytics into a separate function
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/syslog_ng/org/widgets.clj
+++ src/syslog_ng/org/widgets.clj
@@ -253,6 +253,27 @@ log                  { source(s_system); destination(d_all); };"]]]))
      (link-to "http://www.balabit.com/"
               "BalaBit IT Security Ltd")]]])
 
+(defn- page:analytics []
+  (list
+   [:script
+     (str
+      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"
+      "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"
+      "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
+      "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');"
+      "ga('create', 'UA-3350024-43', 'syslog-ng.org');"
+      "ga('send', 'pageview');")]
+   [:script
+     (str
+      "(function(){var didInit = false;function initMunchkin(){"
+      "if(didInit === false) {didInit = true;Munchkin.init('855-UZV-853');}}"
+      "var s = document.createElement('script');s.type = 'text/javascript';"
+      "s.async = true;s.src = '//munchkin.marketo.net/munchkin.js';"
+      "s.onreadystatechange = function() {if (this.readyState == 'complete' "
+      "|| this.readyState == 'loaded') {initMunchkin();}};"
+      "s.onload = initMunchkin;"
+      "document.getElementsByTagName('head')[0].appendChild(s);})();")]))
+
 (defn index []
   (html5
    (page:header)
@@ -269,21 +290,4 @@ log                  { source(s_system); destination(d_all); };"]]]))
     (include-js "//code.jquery.com/jquery-1.10.1.min.js")
     (include-js "bundles/js/all.js")
 
-    [:script
-     (str
-      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"
-      "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"
-      "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
-      "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');"
-      "ga('create', 'UA-3350024-43', 'syslog-ng.org');"
-      "ga('send', 'pageview');")]
-    [:script
-     (str
-      "(function(){var didInit = false;function initMunchkin(){"
-      "if(didInit === false) {didInit = true;Munchkin.init('855-UZV-853');}}"
-      "var s = document.createElement('script');s.type = 'text/javascript';"
-      "s.async = true;s.src = '//munchkin.marketo.net/munchkin.js';"
-      "s.onreadystatechange = function() {if (this.readyState == 'complete' "
-      "|| this.readyState == 'loaded') {initMunchkin();}};"
-      "s.onload = initMunchkin;"
-      "document.getElementsByTagName('head')[0].appendChild(s);})();")]]))
+    (page:analytics)]))
